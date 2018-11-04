fe11c382c24f033004273e1db1171cccfcfdb9ef
Daveyon Mayne <sylarruby@gmail.com>
2018-11-04 09:29:26 +0000

    Removed static firebase config data and replaced to placeholders

--- components/FirebaseIni/FirebaseIni.js
+++ components/FirebaseIni/FirebaseIni.js
@@ -2,14 +2,18 @@ import firebase from 'firebase/app';
 import 'firebase/auth'
 import AuthStateObserver from '../AuthStateObserver'
 
+/**
+ * Initializing the Firebase App
+ * @see https://firebase.google.com/docs/web/setup
+ */
 const FirebaseIni = () => {
 	firebase.initializeApp({
-		apiKey: "AIzaSyBBQ4uEFxsYLJH-_5tEpwaC1DzGKrmaIKk",
-		authDomain: "get-zired.firebaseapp.com",
-		databaseURL: "https://get-zired.firebaseio.com",
-		projectId: "get-zired",
-		storageBucket: "get-zired.appspot.com",
-		messagingSenderId: "108439781601"
+		apiKey: "<API_KEY>",
+		authDomain: "<PROJECT_ID>.firebaseapp.com",
+		databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+		projectId: "<PROJECT_ID>",
+		storageBucket: "<BUCKET>.appspot.com",
+		messagingSenderId: "<SENDER_ID>"
 	});
 
 	firebase.auth().onAuthStateChanged(AuthStateObserver);
